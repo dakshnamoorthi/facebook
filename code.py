67da683bb6bd7f4e9b4473bd99ad6cabafaf8118
dakshnamoorthi <dakssha91@gmail.com>
2022-12-09 08:44:30 +0000

    2nd commit

--- code.py
+++ code.py
@@ -3,6 +3,6 @@ if favorite_season == "Winter":
 elif favorite_season == "Summer":
     print("Summer is amazing")
 elif favorite_season == "Spring":
-    print("I love spring")
+    print("I love ")
 else:
     print("Fall is my mom's favorite season")
