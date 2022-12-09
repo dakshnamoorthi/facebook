b609244492182eeabc0e5e2c714480dbe164bf82
root <root@ip-172-31-8-113.ap-south-1.compute.internal>
2022-12-09 08:37:57 +0000

    initial commit

---
+++ code.py
@@ -0,0 +1,8 @@
+if favorite_season == "Winter":
+    print("That is my favorite season too")
+elif favorite_season == "Summer":
+    print("Summer is amazing")
+elif favorite_season == "Spring":
+    print("I love spring")
+else:
+    print("Fall is my mom's favorite season")
